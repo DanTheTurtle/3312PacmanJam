6000ef0c2f4ec9c2aaab65cd261c67d0ffc818df
Gracen Hoyle <ghoyle@trinity.edu>
2020-09-27 17:35:56 -0500

    Enemies move and kill player; lives system working

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,23 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerControl : MonoBehaviour
 {
 
     public float speed = 10f;
-
+    private int lives;
+    private double spawnX, spawnY;
+    public Text livesText;
 
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<CircleCollider2D>();
+        lives = 3;
+        spawnX = transform.position.x;
+        spawnY = transform.position.y;
+        SetLivesText();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("rejump collided");
+    }
+
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+    }
+
+    void Respawn()
+    {
+        lives -= 1;
+        SetLivesText();
+        transform.position = new Vector3((float)spawnX, (float)spawnY, 0);
+
+        if (lives < 1)
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Respawn();
+        }
     }
 
     // Update is called once per frame
